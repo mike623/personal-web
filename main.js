5e685e80796a6fe905e4869e3c1b3ff08b9acc19
mike_wong <namike623@gmail.com>
2018-08-08 01:48:25 +0800

    fc

---
+++ main.js
@@ -0,0 +1,18 @@
+var options = {
+  strings: [
+    "Font-end geek",
+    "Beck-end expert",
+    "Entrepreneur student",
+    "cook lover",
+    "Sportsman",
+    "Music lover"
+  ],
+  typeSpeed: 80,
+  backDelay: 1000,
+  backSpeed: 40,
+  // shuffle: true,
+  loop: true,
+  loopCount: Infinity,
+}
+
+var typed = new Typed(".typing", options);
