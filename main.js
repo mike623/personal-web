224a216d894e7a40cc968f4066ad334e424ec2ab
mike_wong <namike623@gmail.com>
2018-09-30 03:33:22 +0800

    update style

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("sw.js")
+//     .then(function(registration) {
+//       console.log(
+//         "Service Worker registration successful with scope: ",
+//         registration.scope
+//       );
+//     })
+//     .catch(function(err) {
+//       console.log("Service Worker registration failed: ", err);
+//     });
+// }
+
 var options = {
   strings: [
     "Font-end geek",
     "Beck-end expert",
-    "Entrepreneur student",
     "cook lover",
     "Sportsman",
     "Music lover"
@@ -12,7 +25,38 @@ var options = {
   backSpeed: 40,
   // shuffle: true,
   loop: true,
-  loopCount: Infinity,
-}
+  loopCount: Infinity
+};
 
 var typed = new Typed(".typing", options);
+
+const WordCloud = require('wordcloud')
+
+const skills = require('./skills.json')
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+const list = () => [
+  ...shuffle(skills).map(i=> ([i, getRandomArbitrary(10, 50)]))
+]
+
+
+
+const x = () => WordCloud(document.getElementById('my_canvas'), { list: list(), fontFamily: 'Finger Paint', color: '#c4c4c4' } );
+
+setInterval(x, 1000 * 10)
+x()
