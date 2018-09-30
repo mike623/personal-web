5430a641e26619a7695bfe7dcc229ade50769d15
mike_wong <namike623@gmail.com>
2018-09-30 10:37:08 +0800

    update style

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("sw.js")
-//     .then(function(registration) {
-//       console.log(
-//         "Service Worker registration successful with scope: ",
-//         registration.scope
-//       );
-//     })
-//     .catch(function(err) {
-//       console.log("Service Worker registration failed: ", err);
-//     });
-// }
-
 var options = {
   strings: [
     "Font-end geek",
